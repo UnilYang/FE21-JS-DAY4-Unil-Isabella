656c48552a0baa35f6cf04b203c552b5beb0bdfd
Isabella <isabella.hartwig@gmail.com>
2021-09-28 14:26:14 +0200

    santa ex done

---
+++ Intermediate Bella/inter1_script.js
@@ -0,0 +1,27 @@
+function SantaToggle1() {
+    document.getElementById("dude1").style.opacity = "0";
+    document.getElementById("dude1").style.transition = "1s";
+}
+
+document.getElementById("dude1").addEventListener("click", SantaToggle1)
+
+function SantaToggle2() {
+    document.getElementById("dude2").style.opacity = "0";
+    document.getElementById("dude2").style.transition = "1s";
+}
+
+document.getElementById("dude2").addEventListener("click", SantaToggle2)
+
+function SantaToggle3() {
+    document.getElementById("dude3").style.opacity = "0";
+    document.getElementById("dude3").style.transition = "1s";
+}
+
+document.getElementById("dude3").addEventListener("click", SantaToggle3)
+
+function SantaToggle4() {
+    document.getElementById("dude4").style.opacity = "0";
+    document.getElementById("dude4").style.transition = "1s";
+}
+
+document.getElementById("dude4").addEventListener("click", SantaToggle4)
